f9578f9a8a71e3bfdb9e462bb1adb21698a2defd
pandaCode082 <mahdiroohandeh5@gmail.com>
2024-05-24 22:17:43 +0330

    create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Header from "./Header/Header";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      {React.createElement("h1", { class: "text-4xl text-center font-bold" }, "test create element with react")}
     </div>
   );
 }
