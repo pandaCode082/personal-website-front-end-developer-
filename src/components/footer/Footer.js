9843e616ec0e8e010fbf77b099b0382d348ce041
pandaCode082 <mahdiroohandeh5@gmail.com>
2024-10-15 16:34:03 +0330

    add: footer.js file

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className='relative'>
+            {/* footer defign */}
+            <svg viewBox="0.976 161.014 503.253 148.046" className='absolute top-0 -translate-y-full'>
+                <path style="stroke-linecap: round; stroke: #111111; fill: #252525;" d="M 0.873 309.62 C -34.322 288.42 -17.854 167.715 1.287 161.243 C 20.428 154.771 53.028 211.927 152.038 196.907 C 206.761 188.605 256.343 261.019 280.224 256.501 C 301.337 252.507 339.746 237.191 381.36 234.64 C 418.504 232.363 439.218 268.737 500.09 225.857 C 517.896 213.314 506.844 288.338 501.064 312.706"></path>
+            </svg >
+
+        </footer>
+    )
+}
+
+
+
