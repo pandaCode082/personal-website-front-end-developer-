188a407e76c7628cd69021dd753b3ef4a4a4a256
pandaCode082 <mahdiroohandeh5@gmail.com>
2024-10-28 17:33:36 +0330

    add : responosive information page (aboute.js)

--- src/components/pages/About/Information.js
+++ src/components/pages/About/Information.js
@@ -7,19 +7,31 @@ import { posts } from '../../../postData';
 import { Link } from 'react-router-dom';
 export default function Information() {
     return (
-        <div className='container flex flex-col'>
-            <div className='flex justify-center items-center gap-20'>
-                <div className='flex flex-col gap-3 w-1/3'>
-                    <h1 className='text-5xl drop-shadow-md capitalize font-extrabold'>mahdi roohandeh</h1>
-                    <p className='font-ConcertOneRegular text-gray-800 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero beatae inventore assumenda possimus nam, modi illum, aut ad soluta corporis ipsum, deleniti necessitatibus aspernatur debitis recusandae repellendus! Aliquid, nulla repudiandae libero pariatur veniam, facilis ducimus dolores omnis suscipit facere temporibus accusamus a itaque, dignissimos maiores corporis praesentium maxime assumenda sed distinctio at nesciunt doloremque dolor neque. Facilis cupiditate est ex!</p>
+        <div className='container flex flex-col lg:mt-0 mt-20'>
+            <div className='flex justify-center lg:flex-row flex-col  items-center lg:gap-20 gap-14'>
+                <div className='flex flex-col gap-3 lg:w-1/3 w-[60%] lg:text-left text-center '>
+                    <h1 className='md:text-5xl text-4xl drop-shadow-md capitalize font-extrabold'>mahdi roohandeh</h1>
+                    <p className='font-ConcertOneRegular text-gray-800 text-sm lg:line-clamp-none line-clamp-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero beatae inventore assumenda possimus nam, modi illum, aut ad soluta corporis ipsum, deleniti necessitatibus aspernatur debitis recusandae repellendus! Aliquid, nulla repudiandae libero pariatur veniam, facilis ducimus dolores omnis suscipit facere temporibus accusamus a itaque, dignissimos maiores corporis praesentium maxime assumenda sed distinctio at nesciunt doloremque dolor neque. Facilis cupiditate est ex!</p>
                 </div>
-                <div className='max-w-[500px]'>
+                <div className='max-w-[500px] w-9/12'>
                     <img src="/images/HeaderPanda.png" alt="me" className='drop-shadow-xl shadow-black' />
                 </div>
             </div>
-            <div className='w-full h-72 bg-sky-900/30 backdrop-blur-lg rounded-2xl p-3'>
+
+            <div className='w-full h-72 bg-sky-900/30 backdrop-blur-lg rounded-2xl p-3 text-center'>
                 <Swiper
-                    slidesPerView={3}
+                    breakpoints={{
+                        // when window width is >= 640px
+                        640: {
+                            width: 640,
+                            slidesPerView: 1,
+                        },
+                        // when window width is >= 768px
+                        768: {
+                            width: 768,
+                            slidesPerView: 2,
+                        },
+                    }}
                     spaceBetween={30}
                     pagination={{
                         clickable: true,
@@ -30,7 +42,7 @@ export default function Information() {
                     {
                         posts.map(post => {
                             return (
-                                <SwiperSlide key={post.id} className='bg-white rounded-xl'>
+                                <SwiperSlide key={post.id} className='bg-white rounded-xl aspect-[1/1]'>
                                     <Link to={`/shop/post/${post.id}`} className='w-full h-full  grid place-content-center' >
                                         <div>
                                             <h4>{post.title}</h4>
