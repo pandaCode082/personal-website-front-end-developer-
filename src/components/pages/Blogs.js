4bcb3b7d27424e08b3b23785c6d207592ba2dae3
pandaCode082 <mahdiroohandeh5@gmail.com>
2024-11-07 10:57:48 +0330

    update blogs.js

--- src/components/pages/Blogs.js
+++ src/components/pages/Blogs.js
@@ -1,9 +1,16 @@
 import React from 'react';
 export default function Blogs() {
-
     return (
         <>
-            <h1>bogs</h1>
+            <div className='container h-max mt-8'>
+                <div className='bg-slate-500/50 w-full h-10 rounded-t-2xl flex items-center px-10'>
+                    <button>category 1</button>
+                    <button>category 2</button>
+                    <button>category 3</button>
+                    <button>category 4</button>
+                </div>
+                <div className='w-full bg-black/10'></div>
+            </div>
         </>
     )
 }
